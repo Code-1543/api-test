b08c75bcf68789d4985815214f51eb01af969381
Code-1543 <tanakrit2114@gmail.com>
2024-04-30 20:39:25 +0700

    free ex fetch data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-
-  const [ data, setData ] = useState([])
+  
+  const [ engUsersData, setEngUsersData ] = useState([])
+  const [ thaiUsersData, setThaiUsersData ] = useState([])
+  const [ engProductsData, setEngProductsData ] = useState([])
+  const [ thaiProductsData, setThaiProductsData ] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api-test-2tun.onrender.com/api/users')
-        setData(response.data.users)
+        const response = await axios.get('https://api-test-2tun.onrender.com/free/api')
+        setEngUsersData(response.data.users[0].users_en)
+        setThaiUsersData(response.data.users[0].users_th)
+        setEngProductsData(response.data.products[0].products_en)
+        setThaiProductsData(response.data.products[0].products_th)
       } catch (error) {
         console.log(error)
       }
@@ -17,18 +23,42 @@ const App = () => {
     fetchData()
   }, [])
 
-  return(
+  return (
     <>
       <div>
-        <h1>Users</h1>
-        <ul>
-          {data.map(({ id, name, email }) => (
-            <li key={id}>{name} - {email}</li>
-          ))}
-        </ul>
+        <p>
+          <h1>Eng Users</h1>
+          <ul>
+            {engUsersData.map(({id, name, email}) => (
+              <li key={id}>{name} - {email}</li>
+            ))}
+          </ul>
+        </p>
+        <p>
+          <h1>Thai Users</h1>
+          <ul>
+            {thaiUsersData.map(({id, name, email}) => (
+              <li key={id}>{name} - {email}</li>
+            ))}
+          </ul>
+        </p>
+        <p>
+          <h1>Eng Products</h1>
+          <ul>
+          {engProductsData.map(({product_id, product_name, price}) => (
+              <li key={product_id}>{product_name} - {price}</li>
+            ))}
+          </ul>
+        </p>
+        <p>
+          <h1>Thai Products</h1>
+          <ul>{thaiProductsData.map(({product_id, product_name, price}) => (
+            <li key={product_id}>{product_name} - {price}</li>
+          ))}</ul>
+        </p>
       </div>
     </>
   )
 }
 
-export default App;
+export default App
