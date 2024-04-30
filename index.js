4ea0711d9c409bcf3bbe4ddfdf9ae9e8b59ed243
Code-1543 <tanakrit2114@gmail.com>
2024-04-30 15:52:46 +0700

    add cors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const data = require('./data.json')
-const cors = require('cors')
+const cors = require('cors') 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/api/users', (req, res) => {
-    res.json(data)
+    res.json(data);
 })
 
 app.listen(1111, () => console.log(`SERVER IS RUNNING`))
